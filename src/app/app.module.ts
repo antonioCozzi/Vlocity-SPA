fe44773bff58cd22ef52c8ea08f530cc6c823dc5
Antonio Cozzi <antoniocozzi2@gmail.com>
2018-05-05 17:48:02 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PeopleService } from './people.service';
+import { RouterModule } from '@angular/router';
+import { HeartsComponent } from './hearts/hearts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ProfileComponent,
+    HeartsComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'profile/:id', component: ProfileComponent }
+    ])
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
